d10323ce14c929decf616e14527bc63b775625ce
Kenneth Galbraith <galbrakd@gmail.com>
2021-10-12 22:57:50 -0400

    Did the basic exercises for the Ruby bookX

---
+++ my_folder/the_basics.rb
@@ -0,0 +1,36 @@
+# Exercise 1
+my_name = "Kenneth" + " " + "Galbraith"
+puts "My name is #{my_name}"
+
+# Exercise 2
+my_int = 4567
+thousanths = 4567 / 1000
+hundreths = 4567 % 1000 / 100
+tenths = 4567 % 100 / 10
+ones = 4567 % 10
+puts "My int is #{my_int}, thousanths = #{thousanths}, hundreths = #{hundreths}, tenths = #{tenths}, and ones = #{ones}"
+
+# Exercise 3
+movies = {:the_lighthouse => 2019, :drive => 2011}
+puts movies[:the_lighthouse]
+puts movies[:drive]
+
+# Exercise 4
+my_array = [movies[:the_lighthouse], movies[:drive]]
+puts my_array[0]
+puts my_array[1]
+
+# Exercise 5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# Exercise 6
+puts 4.0**2
+puts 5.0**2
+puts 6.0**2
+
+# Exercise 7
+puts "The error message says I used a right parenthesis instead of a right curly brace"
+
